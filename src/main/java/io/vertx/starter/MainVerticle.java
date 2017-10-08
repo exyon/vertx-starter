8efe481c367311df45b3749b41debabca0258ed8
Shyamal Shah <shyamalshah@Macbook-Pro.local>
2017-10-07 23:49:15 -0400

    workshop - vert.x starter

---
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -0,0 +1,13 @@
+package io.vertx.starter;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start() {
+    //TODO: Create HttpServer and Listen
+
+  }
+
+}
